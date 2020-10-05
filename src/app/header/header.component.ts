78caf741b54b7cd4f86f11c46004cb2724528c84
xiangzhihai <rickyxiangzhihai@gmail.com>
2020-10-05 13:59:39 -0700

    auth status changed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector : "app-header",
   templateUrl : "./header.component.html",
   styleUrls: ["./header.component.css"]
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false
+  constructor(private authService: AuthService) {}
+  private authListenerSubs: Subscription;
+
+  ngOnInit() {
+    this.authListenerSubs = this.authService
+    .getAuthStatusListener()
+    .subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated
+    })
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe()
+  }
+}
