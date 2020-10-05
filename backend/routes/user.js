0e13ca553cff0bd4c25de4c3eeccc8f9ccb5b5c7
xiangzhihai <rickyxiangzhihai@gmail.com>
2020-10-05 13:12:32 -0700

    token send to frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express")
 const bcrypt = require("bcrypt")
 const User = require("../models/user")
 const router = express.Router()
+const jwt = require("jsonwebtoken")
+const user = require("../models/user")
 
 router.post("/signup", (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then(hash => {
@@ -22,5 +24,36 @@ router.post("/signup", (req, res, next) => {
   })
 
 })
+
+router.post("/login", (req, res, next) => {
+  let fetchedUser
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      fetchedUser = user
+      return bcrypt.compare(req.body.password, user.password)
+    }).then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id },
+        "secret_this_should_be_longer",
+        { expiresIn: "1h" })
+      res.status(200).json({
+        token: token
+      })
+    }).catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      })
+    })
+})
+
 
 module.exports = router
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,22 @@ import { Injectable } from '@angular/core';
 import { AuthData } from './auth-data.model';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
   createUser(email: string, password: string) {
-    const authData: AuthData = {email: email, password: password}
-    this.http.post('http://localhost:3000/api/user/signup', authData)
+    const authData: AuthData = { email: email, password: password };
+    this.http
+      .post('http://localhost:3000/api/user/signup', authData)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  login(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
+    this.http.post('http://localhost:3000/api/user/login', authData)
     .subscribe(response => {
       console.log(response)
     })
