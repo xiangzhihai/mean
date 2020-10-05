7f04f3d623186b25cd8eed31c7359dc38fc3db13
xiangzhihai <rickyxiangzhihai@gmail.com>
2020-10-05 11:04:25 -0700

    created user model

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,17 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 
 const postsRoutes = require("./routes/posts")
+const userRoutes = require("./routes/user")
 const Post = require("./models/post")
 
 const app = express()
 
 mongoose.connect("mongodb+srv://ricky:tLyJuPjbp0y68LXE@cluster0.7cm18.mongodb.net/node-angular?retryWrites=true&w=majority")
-.then(() => {
-  console.log("connected to mongodb")
-}).catch(() => {
-  console.log("connection failed")
-})
+  .then(() => {
+    console.log("connected to mongodb")
+  }).catch(() => {
+    console.log("connection failed")
+  })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -21,11 +22,12 @@ app.use("/images", express.static(path.join("backend/images")))
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept")
+    "Origin, X-Requested-With, Content-Type, Accept")
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
   next()
 })
 
 app.use("/api/posts", postsRoutes)
+app.use("/api/user", userRoutes)
 
 module.exports = app
