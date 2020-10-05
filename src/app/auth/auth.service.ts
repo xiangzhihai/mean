0d39107ad48ade5436c96b9dee37a15c3fb9e519
xiangzhihai <rickyxiangzhihai@gmail.com>
2020-10-05 14:41:27 -0700

    logout timer added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
   private isAuthenticated = false;
   private token: string;
   private authStatusListener = new Subject<boolean>();
-
+  private tokenTimer: any
   constructor(private http: HttpClient, private router: Router) {}
 
   getToken() {
@@ -38,13 +38,21 @@ export class AuthService {
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
-      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
+      .post<{ token: string; expiresIn: number }>(
+        'http://localhost:3000/api/user/login',
+        authData
+      )
       .subscribe((response) => {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout()
+          }, expiresInDuration * 1000)
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+
           this.router.navigate(['/']);
         }
       });
@@ -55,5 +63,6 @@ export class AuthService {
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     this.router.navigate(['/']);
+    clearTimeout(this.tokenTimer)
   }
 }
